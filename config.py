bfb749a2ca8a7968df7530975c16aa620a162efd
Srilakshmi k <lakshmi21206@gmail.com>
2026-07-03 01:21:15 +0530

    Reduce max upload file size to 5 MB

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ CONFIG = {
     "image_size": 128,
     "model_path": "model.h5",
     "min_confidence": 70.0,
-    "max_file_size_mb": 10,
+    "max_file_size_mb": 5,
     "min_image_pixels": 32,
 }
